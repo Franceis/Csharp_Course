1cce341f75e89294b510d5d7cda6c03e7756eb92
Vinicius Franca <vinicusfranca10@gmail.com>
2021-04-10 13:09:59 -0300

    BankAccount implemented

---
+++ BankAccount/BankAccount/Account.cs
@@ -0,0 +1,39 @@
+﻿using System.Globalization;
+
+namespace BankAccount {
+    class Account {
+        public int AccountNumber { get; private set; }
+        public string AccountHolder { get; set; }
+
+        public double Balance { get; private set; }
+
+        private double _tax = 5.00;
+
+        public Account(int accountNumber, string accountHolder) {
+            AccountNumber = accountNumber;
+            AccountHolder = accountHolder;
+        }
+
+        public Account(int accountNumber, string accountHolder, double initialDeposit) : this(accountNumber, accountHolder) {
+            Deposit(initialDeposit);
+        }
+
+        public void Deposit(double value) {
+            Balance += value;
+        }
+
+        public void Withdraw(double value) {
+            Balance -= _tax;
+            Balance -= value;
+        }
+
+        public override string ToString() {
+            return "Account "
+                + AccountNumber
+                + ", Holder: "
+                + AccountHolder
+                + ", Balance: $ "
+                + Balance.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ BankAccount/BankAccount/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Globalization;
+
+namespace BankAccount {
+    class Program {
+        static void Main(string[] args) {
+            Account account;
+
+            Console.Write("Enter the account number: ");
+            int accountNumber = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter the account holder name: ");
+            string accountName = Console.ReadLine();
+
+            Console.Write("Will there be an initial deposit (s/n)? ");
+            char answer = char.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            if (answer == 's' || answer == 'S') {
+                Console.Write("Enter the value for initial deposit: ");
+                double initialDeposit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                account = new Account(accountNumber, accountName, initialDeposit);
+            }
+            else {
+                account = new Account(accountNumber, accountName);
+            }
+
+            Console.WriteLine("Account data:");
+            Console.WriteLine(account);
+
+            Console.WriteLine();
+
+            Console.Write("Enter a value to deposit: ");
+            account.Deposit(double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture));
+            Console.WriteLine("Account data actualized:");
+            Console.WriteLine(account);
+
+            Console.WriteLine();
+
+            Console.Write("Enter a value to withdraw: ");
+            account.Withdraw(double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture));
+            Console.WriteLine("Account data actualized: ");
+            Console.WriteLine(account);
+        }
+    }
+}
